2ea069d00e54507007d4bb34a4250891aed35f0a
carissadzr <carissadzr@gmail.com>
2023-10-26 14:00:36 +0700

    feat login register page

---
+++ authentication/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from authentication.views import register #sesuaikan dengan nama fungsi yang dibuat
+from authentication.views import login_user #sesuaikan dengan nama fungsi yang dibuat
+
+app_name = 'authentication'
+
+urlpatterns = [
+    path('register/', register, name='register'), #sesuaikan dengan nama fungsi yang dibuat
+    path('login/', login_user, name='login'),
+]
---
+++ authentication/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render
+from django.shortcuts import redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages  
+from django.contrib.auth import authenticate, login, logout
+import datetime
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
+# Create your views here.
+def register(request): # no button yet
+    form = UserCreationForm()
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your account has been successfully created!')
+            return redirect('main:login')
+    context = {'form':form}
+    return render(request, 'register.html', context)
+
+def login_user(request): # no button yet
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            response = HttpResponseRedirect(reverse("main:show_main")) 
+            response.set_cookie('last_login', str(datetime.datetime.now()))
+            return response
+
+def logout_user(request): # no button yet
+    logout(request)
+    response = HttpResponseRedirect(reverse('main:login'))
+    response.delete_cookie('last_login')
+    return response
