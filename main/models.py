faef6baa0d6d66287de04f24beea4883c6104df4
Akmal76 <akmalramadhan76@gmail.com>
2023-10-26 13:14:18 +0700

    feat: add search and genres filter feature

--- main/models.py
+++ main/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Book(models.Model):
   title       = models.CharField(null=True, blank=True, max_length=255)
@@ -8,3 +9,7 @@ class Book(models.Model):
   genres      = models.TextField(null=True, blank=True)
   cover_img   = models.TextField(null=True, blank=True)
   year        = models.IntegerField(null=True, blank=True)
+  rate        = models.IntegerField()
+
+class Review(models.Model):
+  review = models.ForeignKey(Book, on_delete=models.CASCADE)
--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from main.views import show_main, get_books
+from main.views import show_main, get_books, get_book_json
 
 app_name = 'main'
 
 urlpatterns = [
     path("", show_main, name='show_main'),
+    path("get-book", get_book_json, name="get_book_json"),
     path("api/books", get_books, name="get_books")
 ]
