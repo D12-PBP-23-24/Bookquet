5f00a17580425131de7d5b6c6284a4377a9f7a26
Akmal76 <akmalramadhan76@gmail.com>
2023-10-29 20:23:44 +0700

    feat: make footer as a base.html template

--- main/urls.py
+++ main/urls.py
@@ -10,10 +10,10 @@ urlpatterns = [
     path("find-book", find_book, name="find_book"),
     path('register/', register, name='register'), 
     path('login/', login_user, name='login'),
-    path('logout/', logout_user, name='logout'),
+    path('logout/', logout_user, name='logout_user'),
     path('read-later/add-to-read-later/<int:book_id>/', read_later_book, name="read_later_book"),
     path('toggle-search-feature/', toggle_search_feature, name='toggle_search_feature'),
-    path('toggle-favorite/<int:book_id>/', toggle_favorite_status, name='toggle_favorite'),
+    path('toggle-favorite/<int:book_id>/', toggle_favorite_status, name='toggle_favorite_status'),
 ]
 
 
