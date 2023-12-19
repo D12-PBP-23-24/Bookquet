6e73961085496b369049590a0dec4bbeeb93fe6d
Akmal76 <akmalramadhan76@gmail.com>
2023-12-19 18:27:19 +0700

    fix: Integer for ISBN

--- main/models.py
+++ main/models.py
@@ -5,7 +5,7 @@ class Book(models.Model):
   title        = models.CharField(null=True, blank=True, max_length=255)
   author       = models.CharField(null=True, blank=True, max_length=255)
   description  = models.TextField(null=True, blank=True)
-  isbn         = models.TextField(null=True, blank=True)
+  isbn         = models.IntegerField(null=True, blank=True)
   genres       = models.TextField(null=True, blank=True)
   cover_img    = models.TextField(null=True, blank=True)
   year         = models.IntegerField(null=True, blank=True)
